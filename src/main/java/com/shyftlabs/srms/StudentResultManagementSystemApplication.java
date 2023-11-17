8dcfd8fa759e1c64feda6b4e46e1c87b70cd74a6
Romain <romain.beghi@gmail.com>
2023-11-17 13:57:57 -0500

    Initialization of the project

---
+++ src/main/java/com/shyftlabs/srms/StudentResultManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shyftlabs.srms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentResultManagementSystemApplication.class, args);
+    }
+
+}
