6c8dc4cfb820c5d20dcb54b35d49dd3b0157954f
Romain <romain.beghi@gmail.com>
2023-11-18 20:07:27 -0500

    [feat] first app version with basic display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [clients, setClients] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('clients')
+        .then(response => response.json())
+        .then(data => {
+          setClients(data);
+          setLoading(false);
+        })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Clients List</h2>
+            {clients.map(group =>
+                <div key={group.id}>
+                  {group.name}
+                </div>
+            )}
+          </div>
+        </header>
+      </div>
   );
 }
 
